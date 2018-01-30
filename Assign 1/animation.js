499e395dd833712f0e1f8b3c0e288553e3f508c6
smaccall <sarah.maccallum11@gmail.com>
2018-01-30 14:49:53 -0500

    Finished the input of Art Works

--- Assign 1/animation.js
+++ Assign 1/animation.js
@@ -4,7 +4,25 @@ $(document).ready(function ()
 {
         $("div.content").hide();
         $("#CSC").click(function() {
+                $("div.content").hide();
                 $("#campbells").show();
             }
-        )
-})
+        );
+        $("#SN").click(function() {
+            $("div.content").hide();
+            $("#starry").show();
+        });
+        $("#BV").click(function() {
+            $("div.content").hide();
+            $("#venus").show();
+        });
+        $("#LS").click(function() {
+            $("div.content").hide();
+            $("#supper").show();
+        });
+        $("#WW").click(function() {
+            $("div.content").hide();
+            $("#weeping").show();
+        });
+
+});
