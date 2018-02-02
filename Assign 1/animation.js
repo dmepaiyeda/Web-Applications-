ce6289350cb3974df8f9fed5c983cdc316f66e6c
smaccall <sarah.maccallum11@gmail.com>
2018-02-01 22:23:20 -0500

    Finished the input of Art Works

--- Assign 1/animation.js
+++ Assign 1/animation.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function ()
 {
         $("div.content").hide();
--- Labs/lab2_animation.js
+++ Labs/lab2_animation.js
@@ -1,5 +1,45 @@
 $(document).ready(function()
 {
 
+    $("#fin1").click(function () {
+        $("#prodigal").fadeIn(2000);
+    });
+    $("#fout1").click(function () {
+        $("#prodigal").fadeOut(2000);
+    });
 
+    $("#fin2").click(function () {
+        $("#mona").fadeIn(2000);
+    });
+    $("#fout2").click(function () {
+        $("#mona").fadeOut(2000);
+    });
+
+    $("#fin3").click(function () {
+        $("#bon").fadeIn(2000);
+    });
+    $("#fout3").click(function () {
+        $("#bon").fadeOut(2000);
+    });
+
+    $("#fin11").click(function () {
+        $(".text1").fadeIn(2000);
+    });
+    $("#fout11").click(function () {
+        $(".text1").fadeOut(2000);
+    });
+
+    $("#fin12").click(function () {
+        $(".text2").fadeIn(2000);
+    });
+    $("#fout12").click(function () {
+        $(".text2").fadeOut(2000);
+    });
+
+    $("#fin13").click(function () {
+        $(".text3").fadeIn(2000);
+    });
+    $("#fout13").click(function () {
+        $(".text3").fadeOut(2000);
+    });
 });
