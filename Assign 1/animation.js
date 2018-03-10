1d923437e62eb0201c739f5b016ac3a40fc05bda
Deborah Mepaiyeda <dmepaiyeda@gmail.com>
2018-03-10 15:05:37 -0500

    Created and designed the module

--- Assign 1/animation.js
+++ Assign 1/animation.js
@@ -53,5 +53,28 @@ $(document).ready(function ()
         })
         $("#H").click(function(){
             $("div.content").hide();
-        })
+        });
+
+        var quantity = 0;
+       $('.quantity-plus').click(function(e){
+            // Stop acting like a button
+            e.preventDefault();
+            // Get the field name
+            var quantity = parseInt($('#quantity').val());
+            // If is not undefined
+                $('#quantity').val(quantity + 1);  // Increment
+        });
+
+         $('.quantity-minus').click(function(e){
+            // Stop acting like a button
+            e.preventDefault();
+            // Get the field name
+            var quantity = parseInt($('#quantity').val());
+            // If is not undefined
+                // Increment
+                if(quantity>0){
+                $('#quantity').val(quantity - 1);
+                }
+        });
+
 });
